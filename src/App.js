ea3d0ec6c28627a6cde3b5368730426bbe501157
ceesiyamoah <ccyamoah@gmail.com>
2020-09-26 20:42:07 +0000

    writing pure react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+const Pet = ({ name, animal, breed }) =>
+{
+    return React.createElement('div', {},
+        [
+            React.createElement('h1', {}, name),
+            React.createElement('h2', {}, animal),
+            React.createElement('h2', {}, breed)
+        ])
+}
+
+const App = () =>
+{
+    return React.createElement(
+        'div',
+        {},
+        [React.createElement('h1', {}, 'Adopt me!'),
+        React.createElement(Pet, { name: 'Pepsi', animal: 'Dog', breed: 'Daschund' }),
+        React.createElement(Pet, { name: 'Luna', animal: 'Cat', breed: 'Siamese' }),
+        React.createElement(Pet, { name: 'Els', animal: 'mouse', breed: 'troublesome' })
+        ]
+    );
+};
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('root')
+);
