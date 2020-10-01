84c196458f1a95072997b26634392fa3df8f09d4
ceesiyamoah <ccyamoah@gmail.com>
2020-10-01 01:15:48 +0000

    added carousel, details and reach router

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,51 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <cod>{JSON.stringify(props, null, 4)}</cod>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  state = {
+    loading: true,
+  };
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+  render() {
+    const {
+      name,
+      animal,
+      location,
+      description,
+      breed,
+      loading,
+      media,
+    } = this.state;
+    if (this.state.loading) {
+      return <h1>Loading</h1>;
+    }
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+  return;
+}
 
 export default Details;
