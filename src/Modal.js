047ae8473c541f882695ecf9cb7c19ebadb57406
ceesiyamoah <ccyamoah@gmail.com>
2020-10-01 18:23:50 +0000

    Modal implementation and project completion

---
+++ src/Modal.js
@@ -0,0 +1,19 @@
+import React, { useRef } from "react";
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    const div = document.createElement("div");
+    elRef.current = div;
+  }
+  useEffect(() => {
+    const ModalRoot = document.getElementById("modal");
+    ModalRoot.appendChild(elRef.current);
+    return () => ModalRoot.removeChild(elRef.current);
+  }, []);
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
